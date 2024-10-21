c6554b2d125713d28f9eb357d142a0a8c6189697
Arturex919 <edutuacal@alu.edu.gva.es>
2024-10-21 09:40:50 +0200

    Primer commit tema1

---
+++ src/Tema2_Ejercicos/Tema2_EJ3.java
@@ -0,0 +1,8 @@
+package Tema2_Ejercicos;
+import java.util.Scanner;
+//base y altura de un triangulo
+public class Tema2_EJ3 {
+    public static void main(String[] args) {
+        Scanner
+    }
+}

b574cd45edf280b11b4a5ff4870d1160dc561539
Arturex919 <edutuacal@alu.edu.gva.es>
2024-10-25 14:07:23 +0200

    add ej15

--- src/Tema2_Ejercicos/Tema2_EJ15.java
+++ src/Tema2_Ejercicos/Tema2_EJ15.java
@@ -6,6 +6,7 @@ public class Tema2_EJ15 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int an, mes, dias = 0;
+
         System.out.println("ingrese un numero del mes");
         mes = in.nextInt();
         System.out.println("ingrese el año");

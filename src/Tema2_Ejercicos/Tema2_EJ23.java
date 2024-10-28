1d425ef21e525905791a3fe1b176f986c0530974
Arturex919 <edutuacal@alu.edu.gva.es>
2024-10-28 14:01:02 +0100

    add 28/10

---
+++ src/Tema2_Ejercicos/Tema2_EJ23.java
@@ -0,0 +1,28 @@
+package Tema2_Ejercicos;
+
+import java.util.Scanner;
+
+public class Tema2_EJ23 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int suma = 0, cont = 0;
+        int num;
+        System.out.println("ingrese un numero");
+        num = in.nextInt();
+        while (num<0){
+        suma += num;
+        cont++;
+    }
+
+
+
+
+
+     }
+
+
+
+
+    }
+
+

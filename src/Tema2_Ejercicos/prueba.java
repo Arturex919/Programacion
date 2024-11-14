58e39be901671491e8022bce31d5d839f6e44ab8
Arturex919 <edutuacal@alu.edu.gva.es>
2024-11-14 13:54:54 +0100

    14/11

---
+++ src/Tema2_Ejercicos/prueba.java
@@ -0,0 +1,24 @@
+package Tema2_Ejercicos;
+
+import java.util.Random;
+import java.util.Scanner;
+public class prueba {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+                    }
+}
+
+
+
